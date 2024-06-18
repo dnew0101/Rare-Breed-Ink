bfc45e95710a59b0c8f1abd8c54c879e2ee080ea
David Newman <david.newman.dev1@gmail.com>
2024-06-18 13:06:27 -0700

    Basic Structure; general component tree formatted.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Aftercare from './pages/Aftercare';
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route path="/aftercare" component={Aftercare} />
+      </Switch>
+    </Router>
   )
 }
