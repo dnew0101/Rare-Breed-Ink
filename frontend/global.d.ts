b8e9fd80a1e49a5a28362928b351ee539705a470
David Newman <david.newman.dev1@gmail.com>
2024-07-03 21:06:17 -0700

    Update; frontend to TSX and shadcn/ui integration

---
+++ frontend/global.d.ts
@@ -0,0 +1,16 @@
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
